f12ab987f9decd0925d7af9495e1b2afb672804a
musicakc <amitoj1606@gmail.com>
2015-04-20 23:38:27 +0530

    Reading chapter 1 and implementing code

--- chapter1.py
+++ chapter1.py
@@ -5,3 +5,9 @@ text1.concordance("beautiful")
 text1.similar("beautiful")
 text3.count("ivory")
 sorted(set(text3))
+
+def lexical_diversity(text):
+    return len(text)/ len(set(text))
+
+def percentage(count, total):
+    return 100 * count / total
